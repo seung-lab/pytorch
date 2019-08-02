531d13e7b5a2c29494f9ec2299dd71e2add50f01
Supriya Rao <supriyar@fb.com>
2019-08-01 17:34:37 -0700

    Add support for using caffe2::ThreadPool in pytorch mobile QNNPACK.
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/23658
    
    caffe2::ThreadPool is optimized for mobile. This change leverages this logic for pytorch mobile. It is guarded by the C10_MOBILE macro.
    For server side we return nullptr.
    ghstack-source-id: 87604099
    
    Differential Revision: [D16594020](https://our.internmc.facebook.com/intern/diff/D16594020/)

---
+++ aten/src/ATen/native/quantized/cpu/qnnpack_utils.cpp
@@ -0,0 +1,18 @@
+#include <ATen/native/quantized/cpu/qnnpack_utils.h>
+
+#if(defined C10_MOBILE && defined USE_QNNPACK)
+std::unique_ptr<caffe2::ThreadPool> ThreadPoolMobile::thread_pool_ = nullptr;
+std::mutex ThreadPoolMobile::thread_pool_creation_mutex_;
+
+pthreadpool_t ThreadPoolMobile::qnnpack_threadpool() {
+    std::lock_guard<std::mutex> guard(thread_pool_creation_mutex_);
+    if (!thread_pool_) {
+      thread_pool_ = caffe2::ThreadPool::defaultThreadPool();
+    }
+    return reinterpret_cast<pthreadpool_t>(thread_pool_.get());
+}
+#else
+pthreadpool_t ThreadPoolMobile::qnnpack_threadpool() {
+  return nullptr;
+}
+#endif
